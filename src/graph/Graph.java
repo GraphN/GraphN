ea55700a9fab87bf3aba9a78dae19fa2cc9af3b9
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-03-22 15:20:30 +0100

    Modifications vertex et graph .java

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,8 +1,17 @@
 package graph;
 
+import java.util.ArrayList;
+
 /**
  * Created by Adrian on 22.03.2017.
  */
 public abstract class Graph {
+    private static int n;
+    private ArrayList<Vertex> vertex;
 
+    public Graph(int nVertex){
+        int n = 0;
+        for(int i = 0; i < nVertex; i++)
+            vertex.add(new Vertex(n++));
+    }
 }
--- src/graph/Vertex.java
+++ src/graph/Vertex.java
@@ -1,10 +1,28 @@
 package graph;
 
+import java.util.ArrayList;
+
 /**
  * Created by Adrian on 22.03.2017.
  */
 public class Vertex {
-    int id;
-    String name;
-    
+    private int id;
+    private String name;
+
+    public Vertex(int id){
+        this.id = id;
+    }
+
+    public Vertex(int id, String name){
+        this(id);
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
