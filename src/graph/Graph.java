9c63c4559b2dd3b47e818dc886dc38a04cc3f248
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-03-22 14:37:21 +0100

    Création des classes graph et vertex

---
+++ src/graph/Graph.java
@@ -0,0 +1,8 @@
+package graph;
+
+/**
+ * Created by Adrian on 22.03.2017.
+ */
+public abstract class Graph {
+
+}
---
+++ src/graph/Vertex.java
@@ -0,0 +1,10 @@
+package graph;
+
+/**
+ * Created by Adrian on 22.03.2017.
+ */
+public class Vertex {
+    int id;
+    String name;
+    
+}
