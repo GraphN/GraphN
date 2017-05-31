443bee5a98eeec2c7989be5ab63c2728194af13a
francois.quellec <francois.quellec@heig-vd.ch>
2017-05-31 13:46:37 +0200

    Fix prim update vertex weight

--- src/Algorithms/Prim.java
+++ src/Algorithms/Prim.java
@@ -57,7 +57,7 @@ public class Prim implements AlgorithmVisitor{
                 step.setMessage(message);
                 step.setStructures(structures);
                 g.getVertex(w).setDescription(distTo[w] + "");
-                //step.setVertex(g.getVertex(w));
+                step.setVertex(g.getVertex(w));
 
                 path.add(step);
 
